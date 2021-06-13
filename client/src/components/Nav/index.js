0edc3e79cdc0f52ebfeb61d1a46622912333c6c4
Callie Nipper <callie.nipper@gmail.com>
2021-06-13 15:47:51 -0700

    styled nav bar. Added bootstrap and custom fonts

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,15 +1,37 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
+import "./styles.css";
 
 
 const Nav = () => {
     return (
-        <div>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/projects">Projects</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/connect">Connect</NavLink>
-        </div>
+        <header>
+            <nav className="navbar navbar-expand-sm">
+                <div class="container-fluid">
+                    <nav>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"><i className="fas fa-bars"></i></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
+                                    <NavLink to="/">Home</NavLink>
+                                </li>
+                                <li class="nav-item">
+                                    <NavLink to="/projects">Projects</NavLink>
+                                </li>
+                                <li class="nav-item">
+                                    <NavLink to="/about">About</NavLink>
+                                </li>
+                                <li class="nav-item">
+                                    <NavLink to="/connect">Connect</NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </nav>
+        </header>
     )
 }
 
