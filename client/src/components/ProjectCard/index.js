3c10e57301413f4e7c240a932c9d4cd69bb8552c
Callie Nipper <callie.nipper@gmail.com>
2021-06-19 16:34:19 -0700

    Styled the connect icon

--- client/src/components/ProjectCard/index.js
+++ client/src/components/ProjectCard/index.js
@@ -7,12 +7,14 @@ const ProjectCard = (props) => {
         <div className="col-md-4 col-sm-6">
             <div className="card">
                 <div className="img-container">
-                    <img alt={props.name} src={props.image} />
+                    <a href={props.deployed}><img alt={props.name} src={props.image} /></a>
                 </div>
                 <div className="card-body">
-                    <h3>{props.name}</h3>
-                    <a href={props.github} target="_blank" rel="noopener noreferrer"><img className="gitcat" src="../../assets/github.png" alt="GitHub Logo" /></a>
-                    <a href={props.deployed} target="_blank" rel="noopener noreferrer"><i className="deploy fas fa-external-link-alt"></i></a>
+                    <a href={props.deployed}><h3>{props.name}</h3></a>
+                    <div className="icons">
+                        <a href={props.github} target="_blank" rel="noopener noreferrer"><img className="gitcat" src="../../assets/github.png" alt="GitHub Logo" /></a>
+                        <a href={props.deployed} target="_blank" rel="noopener noreferrer"><i className="deploy fas fa-external-link-alt"></i></a>
+                    </div>
                 </div>
             </div>
         </div>
