569102cf34320423c71e9b62aeb26c8c0b5dc8a4
Callie Nipper <callie.nipper@gmail.com>
2021-06-19 15:59:42 -0700

    Worked on the contact pahe

--- client/src/components/Connect/index.js
+++ client/src/components/Connect/index.js
@@ -1,11 +1,17 @@
 import React from "react";
+import "./style.css";
 
 
 function Connect() {
 
     return (
-        <div>
-            <h1>Let&#8217;s Connect</h1>
+        <div className="connect-back">
+            <h1 className="title">Let&#8217;s Connect</h1>
+            <div className="icons">
+                <a href="https://github.com/calliebn" target="_blank" rel="noopener noreferrer"><img className="icons" src="../../assets/GitHub-Mark-120px-plus.png" alt="GitHub Logo" /></a>
+                <a href="https://www.linkedin.com/in/callie-nipper-7b20406/" target="_blank" rel="noopener noreferrer"><img className="icons" src="../../assets/LI-In-Bug.png" alt="LinkedIn Logo" /></a>
+                <a href="mailto:callie.nipper@gmail.com"><i className="far fa-envelope mailcall"></i></a>
+            </div>
         </div>
 
     );
