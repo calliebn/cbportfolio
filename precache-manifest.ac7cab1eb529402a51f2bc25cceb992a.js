f61041f79d11e9308a732eee87f38f689b954245
Callie Nipper <callie.nipper@gmail.com>
2021-06-26 09:49:42 -0700

    Updates

---
+++ precache-manifest.ac7cab1eb529402a51f2bc25cceb992a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ba1d46b314f17cdc08e2c1f2f96f66",
+    "url": "/cbportfolio/index.html"
+  },
+  {
+    "revision": "c145a2ae14d36000f10d",
+    "url": "/cbportfolio/static/css/main.ee0c4b18.chunk.css"
+  },
+  {
+    "revision": "abd6de39805e8712cf09",
+    "url": "/cbportfolio/static/js/2.64968be4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/cbportfolio/static/js/2.64968be4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c145a2ae14d36000f10d",
+    "url": "/cbportfolio/static/js/main.bf34a1bf.chunk.js"
+  },
+  {
+    "revision": "fa8e72e638b0f9e6ff8c",
+    "url": "/cbportfolio/static/js/runtime-main.ee660ed4.js"
+  }
+]);
