17519be59918f74689a424be96dc1e5afc6a6b3f
Callie Nipper <callie.nipper@gmail.com>
2021-06-27 14:32:29 -0700

    Played with the CSS and content on Nav bar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -8,30 +8,27 @@ const Nav = () => {
         <header>
             <nav className="navbar navbar-expand-sm">
                 <div className="container-fluid">
-                    <nav>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"><i className="fas fa-bars"></i></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <NavLink to="/">Home</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink to="/projects">Projects</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink to="/about">About</NavLink>
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink to="/connect">Connect</NavLink>
-                                </li>
-                            </ul>
+                    <div className="navbar-brand"><NavLink to="/">Callie Nipper</NavLink></div>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"><i className="fas fa-bars"></i></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <div className="navbar-nav">
+                            <div className="nav-item">
+                                <NavLink to="/projects">Projects</NavLink>
+                            </div>
+                            <div className="nav-item">
+                                <NavLink to="/about">About</NavLink>
+                            </div>
+                            <div className="nav-item">
+                                <NavLink to="/connect">Connect</NavLink>
+                            </div>
                         </div>
-                    </nav>
+                    </div>
+
                 </div>
             </nav>
-        </header>
+        </header >
     )
 }
 
